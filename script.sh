7f173661584332e3ba417bb4175aa7c24232d1c9
TCStephane <t.stephane@alustudent.com>
2026-02-05 09:10:09 +0000

    initial commit

--- script.sh
+++ script.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/bash
 
-TEMP_FILE="/TMP/data_processing.txt"
+TEMP_FILE="/tmp/data_processing.txt"
+
 cleanup() {
     echo "Caught signal! let's archive current file..."
     TIMESTAMP=$(date '+%Y-%m-%d')
     ARCHIVE_NAME="archive_file_$TIMESTAMP.txt"
     mv "$TEMP_FILE" "$ARCHIVE_NAME"
 
-    touch "Save the old file and created a new one
+    touch "Save the old file and created a new one"
     
     echo "Saved $TEMP_FILE file and createed a new one"
 }
 
-trap cleaner SIGINT
+trap cleanup SIGINT
 
 echo "Writing timestamp to $TEMP_FILE | Press ctrl_c tp arcive"
-touch "$TEMP_FILE
+touch "$TEMP_FILE"
 while true; do 
-    echo "Log entry: $(date)" >> $TEMP_FILE
-    sleep s
-cleanup
+    echo "Log entry: $(date)" >> "$TEMP_FILE"
+    sleep 1
+done
